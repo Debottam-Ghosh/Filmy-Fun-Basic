78f877ddb5e958c04b3f0d551cca5201b00c5ca2
Debottam Ghosh <debottamghosh361@gmail.com>
2025-05-31 15:03:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,42 +10,29 @@ from keras_vggface.vggface import VGGFace
 from sklearn.metrics.pairwise import cosine_similarity
 import gdown
 
-# Download and load the embedding.pkl from Google Drive
-file_id = '1LdBa-zfv3LmufXZA87MRWHf9BgCQHzpz'
-url = f'https://drive.google.com/uc?id={file_id}'
-
-# Output file name
-output = 'embedding.pkl'
-
-# Download file
-gdown.download(url, output, quiet=False)
-
-# Load the pickle file
-with open(output, 'rb') as f:
-    feature_list = pickle.load(f)
-
-
 st.title("Which Bollywood Celebrity Do You Look Like???")
-
-model = VGGFace(model='resnet50', include_top=False, input_shape=(224, 224, 3), pooling='avg')
-filenames = pickle.load(open('filenames.pkl', 'rb'))
-
 
 def save_your_image(your_image):
     try:
-        with open(os.path.join('uploads',your_image.name), 'wb') as f:
+        with open(os.path.join('uploads', your_image.name), 'wb') as f:
             f.write(your_image.getbuffer())
         return True
     except:
         return False
 
-
-def extract_features(img_path, model):
-
+@st.cache_resource
+def load_model_and_detector():
+    model = VGGFace(model='resnet50', include_top=False, input_shape=(224, 224, 3), pooling='avg')
     detector = MTCNN()
-    test_image = img_path
-    sample_img = cv2.imread(test_image)
+    return model, detector
+
+@st.cache_resource
+def extract_features(img_path):
+    model, detector = load_model_and_detector()
+    sample_img = cv2.imread(img_path)
     result = detector.detect_faces(sample_img)
+    if not result:
+        return None
     x, y, width, height = result[0]['box']
     face = sample_img[y:y + height, x:x + width]
     image = Image.fromarray(face)
@@ -54,35 +41,51 @@ def extract_features(img_path, model):
     expanded_img = np.expand_dims(face_array, axis=0)
     preprocessed_img = preprocess_input(expanded_img)
     result = model.predict(preprocessed_img).flatten()
-
     return result
 
+def load_embeddings():
+    if "feature_list" not in st.session_state:
+        file_id = '1LdBa-zfv3LmufXZA87MRWHf9BgCQHzpz'
+        url = f'https://drive.google.com/uc?id={file_id}'
+        output = 'embedding.pkl'
+        gdown.download(url, output, quiet=False)
+        with open(output, 'rb') as f:
+            st.session_state.feature_list = pickle.load(f)
+    return st.session_state.feature_list
+
+@st.cache_data
+def load_filenames():
+    with open('filenames.pkl', 'rb') as f:
+        return pickle.load(f)
+
 def recommend(feature_list, feature):
-    similarity = []
-    for i in range(len(feature_list)):
-        similarity.append(cosine_similarity(feature.reshape(1, -1), feature_list[i].reshape(1, -1))[0][0])
-
-    index_pos = sorted(list(enumerate(similarity)), reverse=True, key=lambda x: x[1])[0][0]
+    similarity = [
+        cosine_similarity(feature.reshape(1, -1), feat.reshape(1, -1))[0][0]
+        for feat in feature_list
+    ]
+    index_pos = sorted(enumerate(similarity), key=lambda x: x[1], reverse=True)[0][0]
     return index_pos
-
-
 
 your_image = st.file_uploader("Choose your image :)")
 
 if your_image is not None:
     if save_your_image(your_image):
         display_image = Image.open(your_image)
-        features = extract_features(os.path.join('uploads', your_image.name), model)
-        index_pos = recommend(feature_list,features)
-        display_output = Image.open(filenames[index_pos])
+        features = extract_features(os.path.join('uploads', your_image.name))
+        if features is None:
+            st.error("No face detected in the image.")
+        else:
+            feature_list = load_embeddings()
+            filenames = load_filenames()
+            index_pos = recommend(feature_list, features)
+            display_output = Image.open(filenames[index_pos])
 
-        col1, col2 = st.columns(2)
-        with col1:
-            st.header("Your uploaded image")
-            st.image(display_image )
+            col1, col2 = st.columns(2)
+            with col1:
+                st.header("Your uploaded image")
+                st.image(display_image)
 
-        with col2:
-            celebrity = " ".join(filenames[index_pos].split('\\')[1].split('_'))
-            st.header(f"You look like {celebrity}")
-            st.image(display_output)
-
+            with col2:
+                celebrity = " ".join(filenames[index_pos].split('\\')[1].split('_'))
+                st.header(f"You look like {celebrity}")
+                st.image(display_output)
